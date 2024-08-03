b32a695c26c230c7d72253d71e77a25c5995bf18
Bharath1702 <bharathac7@gmail.com>
2024-08-03 19:24:06 +0530

    new

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const { app, server } = require('./socket/index')
 // const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200,
     credentials : true
 }))
 app.use(express.json())
